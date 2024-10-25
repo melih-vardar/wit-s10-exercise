cc37e84cd5321719f4e9047ab1fa163a2f4d03ae
Melih Vardar <mvardar97@gmail.com>
2024-10-25 13:13:58 +0300

    login-text-edit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -38,7 +38,7 @@ function Login({ setUser }) {
         <form 
         onSubmit={handleSubmit(submitFn)}
         className="space-y-6">
-          <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign in</h5>
+          <h5 class="text-xl font-medium text-gray-900 dark:text-white">Create Your Own Team!!</h5>
         <div>
           <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
             Email:
@@ -74,7 +74,7 @@ function Login({ setUser }) {
           <div>
             <button disabled={!isValid} 
             className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              Giriş yap
+              Log in
             </button>
             </div>       
         </form>
